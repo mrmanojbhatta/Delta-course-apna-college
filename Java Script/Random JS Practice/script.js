7cac590e2d7bbbee8c031daae514bc45459d12ec
Manoj Bhatta <mrmanojbhatta@gmail.com>
2026-01-03 19:52:30 +0545

    random practice

---
+++ Java Script/Random JS Practice/script.js
@@ -0,0 +1,111 @@
+// let str="apple";
+// if((str[0]=='a')&&(str.length>3)){
+//     console.log("Good String");
+// }
+// else{
+//     console.log("Not a good String")
+// }
+
+// let num = 12;
+// if ((num % 3 == 0) && ((num + 1 == 15) || (num - 1 == 11))) {
+//     console.log("safe");
+// }
+// else {
+//     console.log("Unsafe");
+// }
+// let day=3;
+// switch(day){
+//     case 1:
+//         console.log("Sunday");
+//         break;
+//         case 2:
+//         console.log("Monday");
+//          break;
+//         case 3:
+//         console.log("Tuesday");
+//          break;
+//         case 4:
+//         console.log("Wednesday");
+//          break;
+//         case 5:
+//         console.log("Thursday");
+//          break;
+//         case 6:
+//         console.log("Friday");
+//          break;
+//         case 7:
+//         console.log("Saturday");
+//         default:
+//             console.log("You enter wrong day");
+// }
+
+
+// // alert("Something is wrong!")
+// // prompt("Please enter your roll no");
+// // console.error("this is a error message");
+// // console.warn("this is a warning msg");
+
+// let fname = prompt("Enter First name:");
+// let lname = prompt("Enter Last name:");
+// let msg=("Welcome " + fname +"" + lname + "!");
+// alert(msg);
+
+
+// let friends=["shishir","sewi","shimran","kabiraj","saraswati"];
+// let nums=[2,4,6,8];
+// let fruites=["apple","mango","banana","litchi"];
+
+// let months=["january","july","march","august"];
+
+// let lang = ["c", "c++", "html", "js", "python", "java"];
+
+// for(let i=1; i<=15; i=i+2){
+//     console.log(i)
+// }
+
+// for(let i=15; i>=1; i=i-2){
+//     console.log(i);
+// }
+
+// let n=prompt("Enter a number for mul");
+// n=parseInt(n);
+// for(i=n; i<=n*10; i=i+n){
+// console.log(i);
+// }
+// const favMovie="avatar";
+// let guess=prompt("Guess my favourite movie");
+// while((guess !=favMovie)&&(guess !="quit")){
+//     guess=prompt("Wrong guess. Please try again");
+// }
+// if(guess==favMovie){
+//     console.log("Congratulations")
+// }
+// else{
+//     console.log("you quit")
+// }
+
+// let fruites=["Apple", "Mango","Banana"];
+// for(i=0; i>3; i++){
+//     console.log(i, fruites[i])
+// }
+
+// let fruites=["apple", "banana","orange","litchi"];
+// for(fruit of fruites){
+//     console.log(fruit)
+// }
+// const student={
+//     name:"Manoj",
+//     class:10,
+//     roll:1,
+//     city:"Ghodaghodi"
+// };
+const classInfo={
+    aman:{
+        grade:"A+",
+        city:"Delhi"
+    },
+    shradha:{
+        grade:"A",
+        city:"Pune"
+    }
+};
