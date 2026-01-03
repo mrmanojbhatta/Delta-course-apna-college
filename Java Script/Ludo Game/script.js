8cc030360cc41caa65bdd872cdeac23c0d2ce512
Manoj Bhatta <mrmanojbhatta@gmail.com>
2026-01-03 22:03:38 +0545

    Implement rollDice function to simulate dice roll

---
+++ Java Script/Ludo Game/script.js
@@ -0,0 +1,11 @@
+// function printPoem(){
+//     console.log("Twinkle Twikle, Little Star");
+// }
+// printPoem();
+
+// create a function roll a dice & always display the value of the dice(1,6)
+function rollDice(){
+let rand=Math.floor(Math.random()*6)+1;
+        console.log(rand);
+}
+rollDice();
