38d889c49e0c86ea48619320446732d0a46038b9
Manoj Bhatta <mrmanojbhatta@gmail.com>
2026-01-06 19:32:57 +0545

    Add scope example with greet variable

---
+++ Java Script/Practice Qs/Scope/script.js
@@ -0,0 +1,10 @@
+let greet = "Hello";
+function changeCreet() {
+    let greet = "namaste";
+    console.log(greet);
+    function innerGreen() {
+        console.log(greet);
+    }
+}
+console.log(greet);
+changeCreet(Greet);
