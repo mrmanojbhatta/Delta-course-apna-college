fbcd1dec21b8fccfd4c475004adcd476d2942a14
Manoj Bhatta <mrmanojbhatta@gmail.com>
2026-01-07 20:20:12 +0545

    Add student array and forEach logging

---
+++ Java Script/Arr forEach/student.js
@@ -0,0 +1,17 @@
+let arr = [
+    {
+        name: "Manoj Bhatta",
+        marks: 98,
+    },
+    {
+        name: "Salman Khan",
+        marks: 74,
+    },
+    {
+        name: "Ramesh Saud",
+        marks: 80,
+    },
+];
+arr.forEach((student) => {
+    console.log(student);
+})
